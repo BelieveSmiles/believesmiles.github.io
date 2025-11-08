816fd7159888f3e543365dfe9d64c609f33ebb1a
BelieveSmiles <3976466282@qq.com>
2025-11-08 22:28:54 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-// 处理所有页面加载情况
-window.addEventListener('beforeunload', () => {
-    // 在页面卸载前强制滚动到顶部
-    window.scrollTo(0, 0);
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    // 兼容异步加载内容
-    requestAnimationFrame(() => {
-        window.scrollTo({ top: 0, behavior: 'instant' });
-    });
-});
 // 顶部视频随机播放
 const mp4_list = [
     "Naruto-White-Kurama-4K.mp4",
